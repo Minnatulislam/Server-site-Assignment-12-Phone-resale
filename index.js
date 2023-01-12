b6368920485371d87283a6c8d783e9e2e6bc238f
Minnatulislam <minnatulislam86@gmail.com>
2023-01-12 21:24:22 +0600

    edited firebase config.js in server site

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const app = express()
 const stripe = require("stripe")(process.env.STRIPE_SCRET_KEY);
 const port = process.env.PORT || 5000 ;
 
-//midler awere;
+//midler awere--------;
 
 app.use(cors())
 app.use(express.json())
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2ulkcud.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2ulkcud.mongodb.net/?retryWrites=true&w=majority`;
  
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
